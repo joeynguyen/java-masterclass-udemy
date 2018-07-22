af10d41a163dd9365e202a5eea29f0cd486362f1
Joey Nguyen <jnn193@gmail.com>
2018-07-22 02:40:05 -0500

    Add char example

---
+++ CharAndBoolean/src/com/joeynguyen/Main.java
@@ -0,0 +1,11 @@
+package com.joeynguyen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // char has a width of 16 (2 bytes)
+	    char myChar = '\u00A9';
+	    System.out.println("Unicode output: " + myChar);
+
+    }
+}
