f3d6762cd76ab8b5b2e59d8aaad110e60dce2b6a
Joey Nguyen <jnn193@gmail.com>
2018-07-22 03:00:21 -0500

    Add String example

---
+++ Strings/src/com/joeynguyen/Main.java
@@ -0,0 +1,14 @@
+package com.joeynguyen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + " and this is more";
+        System.out.println(myString);
+        int myInt = 5010;
+        myString = myString + myInt;
+        System.out.println(myString);
+    }
+}
