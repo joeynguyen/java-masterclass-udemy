5b44b277540ac9de2bea1bd142a0522690ffc3a9
Joey Nguyen <jnn193@gmail.com>
2018-08-05 17:30:42 -0500

    Updated method to return a value

--- Methods/src/com/joeynguyen/Main.java
+++ Methods/src/com/joeynguyen/Main.java
@@ -3,17 +3,24 @@ package com.joeynguyen;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore(true, 800, 5, 100);
-        
-        calculateScore(true, 10000, 8, 200);
+        int finalScore = calculateScore(false, 800, 5, 100);
+        System.out.println("Your initial score was: " + finalScore);
+
+        finalScore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was: " + finalScore);
+
+        finalScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was: " + finalScore);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was: " + finalScore);
+            return finalScore;
         }
+
+        return -1;
     }
 }
